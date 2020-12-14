2811a1906a8119f887406b466826dde1360a837d
Станислав Микулич <S:\TheBat\App\TheBat\Mail>
2020-12-14 16:01:04 +0300

    Yfxfnf ghjuhfvvf 113

---
+++ 1-1/Информатика/2/zad113/UMain.pas
@@ -0,0 +1,41 @@
+unit UMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Timer1: TTimer;
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    Button1: TButton;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+{
+На канве компонента Image при нажатии кнопки «Рисовать»
+построите изображение кораблика. Использовать рисование
+по точкам. При помощи компонентов RadioButton задавать
+способ вывода изображения – мгновенно, замедленно.
+Для замедления использовать компонент Timer. Выполните
+задание, добавив, если необходимо, защиту от неправильного
+ввода данных. Форма обязательно должна содержать кнопку
+для закрытия приложения, кнопку очистки компонентов и
+главное меню, дублирующее основные действия.
+}
+end.
