2d9d69ab8ee8b7a8221fe0d2bed09fca639b0897
stanislaumich <staslz42stas@gmail.com>
2021-08-26 14:56:07 +0300

    AUTO FROM WORK 26.08.2021 14:56:04,15

---
+++ 2-kurs/2-1/Конструирование и языки/Инкин/ConsoleApp73/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 73 Напишите программу, которая выводит на экран таблицу значений функции y=sqrt(abs(2x-5))
+ * в диапазоне от -2 до 2. Шаг изменения аргумента 0,1.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            double x = -2;
+            double step = .1;
+            do
+            {
+                y = Math.Sqrt(Math.Abs(2*x-5));
+                Console.WriteLine("x={0}   y={1}", Math.Round(x,2), Math.Round(y,2));
+                x += step;
+            }
+            while (x <= 2.1);
+            Console.WriteLine("Работа завершена, нажмите ВВОД");
+            Console.Read();
+        }
+    }
+}
---
+++ 2-kurs/2-1/Конструирование и языки/Ольга/ConsoleApp67/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 67 Вычислите приближенно значение бесконечной суммы с точностью до Е
+ * S=1/(2*3)+1/(3*4),,,,,
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s=0;// значение функции
+            int x = 2;// начальное значение
+            int step = 1;// шаг
+            do
+            {
+                s += 1/(x*(x+1)); //вычисляем значение функции
+                Console.WriteLine("ШАГ={0}   y={1}",x-1, y );// выводим результат
+                x += step;// приращаем аргумент шага
+            }
+            while (x <= 2.1);// проверяем условие окончания
+            Console.WriteLine("Работа завершена, нажмите ВВОД");
+            Console.Read();
+        }
+    }
+}
