be4e0759040523ea45e8a3fb37886b4e06da4fcf
stanislaumich <staslz42stas@gmail.com>
2021-08-27 14:25:23 +0300

    AUTO FROM WORK 27.08.2021 14:25:19,84

--- 2-kurs/2-1/Конструирование и языки/Ольга/ConsoleApp67/Program.cs
+++ 2-kurs/2-1/Конструирование и языки/Ольга/ConsoleApp67/Program.cs
@@ -15,15 +15,26 @@ namespace ConsoleApp67
         static void Main(string[] args)
         {
             double s=0;// значение функции
+            double sp = 0;// предыдущее значение функции
             int x = 2;// начальное значение
             int step = 1;// шаг
+            double c = 0;
+            Console.WriteLine("Введите нужную точность:");
+            double e= Convert.ToDouble(Console.ReadLine());
+            c = x * (x + 1);
+            s = s + 1 / c; //вычисляем значение функции
+            Console.WriteLine("ШАГ={0}   S={1} SP={2} SP-S={3}", x - 1, s, sp, sp - s);// выводим результат
+            x += step;// приращаем аргумент шага
+            sp = s;
             do
             {
-                s += 1/(x*(x+1)); //вычисляем значение функции
-                Console.WriteLine("ШАГ={0}   y={1}",x-1, y );// выводим результат
-                x += step;// приращаем аргумент шага
+                c = x * (x + 1);
+                sp = s;
+                s =  s+ 1/c; //вычисляем значение функции
+                Console.WriteLine("ШАГ={0}   S={1}  delta={2}", x-1, s,Math.Abs(sp-s) );// выводим результат
+                x += step;// приращаем аргумент шага                
             }
-            while (x <= 2.1);// проверяем условие окончания
+            while (Math.Abs(sp-s) > e);// проверяем условие окончания
             Console.WriteLine("Работа завершена, нажмите ВВОД");
             Console.Read();
         }
