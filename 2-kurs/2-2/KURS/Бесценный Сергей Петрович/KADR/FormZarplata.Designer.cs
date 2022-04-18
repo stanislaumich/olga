48f958c8cff66983f211e13cf54668e1997f4060
Станислав Микулич <staslz42stas@gmail.com>
2022-04-18 16:02:24 +0300

    AUTO FROM WORK 18.04.2022 16:02:23,58

--- 2-kurs/2-2/KURS/Бесценный Сергей Петрович/KADR/FormZarplata.Designer.cs
+++ 2-kurs/2-2/KURS/Бесценный Сергей Петрович/KADR/FormZarplata.Designer.cs
@@ -29,10 +29,17 @@ namespace KADR
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormZarplata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormZarplata";
+            this.ClientSize = new System.Drawing.Size(866, 650);
+            this.Name = "FormZarplata";
+            this.Text = "Зарплата сотрудников";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
