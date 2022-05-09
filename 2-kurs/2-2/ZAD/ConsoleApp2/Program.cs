f94f7b3b89dc40b2d95709df08814562b87b9ed6
stanislaumich <staslz42stas@gmail.com>
2022-05-09 21:27:38 +0300

    AUTO FROM WORK 09.05.2022 21:27:37,27

---
+++ 2-kurs/2-2/ZAD/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, t, p1, p2, p3;
+            x = 14.26;
+            y = -1.22;
+            z = 3.5e-2;
+            t=.564849;
+
+            p1 = 2 * Math.Cos(x - Math.PI / 6);
+            p2 = .5 + Math.Pow(Math.Sin(y), 2);
+            p3 = 1 + Math.Pow(z,2) / (3-(Math.Pow(z,2)/5));
+            t = p1 / p2 * p3;
+            Console.WriteLine(t);
+        }
+    
+    }
+}
