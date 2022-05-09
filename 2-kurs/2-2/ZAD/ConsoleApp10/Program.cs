a22afdab2a2952fbb0df04bde5d3bd03403aa009
stanislaumich <staslz42stas@gmail.com>
2022-05-09 22:23:19 +0300

    AUTO FROM WORK 09.05.2022 22:23:18,05

---
+++ 2-kurs/2-2/ZAD/ConsoleApp10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Insert N=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            var a = new double[20];
+            double m, s;
+            int strt, stp;
+            strt = 0;
+            stp = 0;
+            m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Insert N[");
+                Console.Write(i);
+                Console.Write("]=");
+                a[i] = Convert.ToDouble(Console.ReadLine());
+                if (i == 0) { m = a[i]; }
+                else
+                {
+                    if (a[i] < m) { m = a[i]; }
+                }
+
+            }
+            Console.Write("Min="); Console.WriteLine(m);
+            for (int i = 0; i < n; i++)
+            {
+               if (a[i] > 0) { strt = i+1;break; } 
+
+            }
+            for (int i = n; i >=0; i--)
+            {
+                if (a[i] > 0) { stp = i-1; break; }
+
+            }
+            Console.Write("Start="); Console.WriteLine(strt);
+            Console.Write("Stop="); Console.WriteLine(stp);
+            s = 0;
+            for(int i = strt; i <= stp; i++)
+            {
+                s += a[i];
+            }
+            Console.Write("SUM="); Console.WriteLine(s);
+        }
+    }
+}
