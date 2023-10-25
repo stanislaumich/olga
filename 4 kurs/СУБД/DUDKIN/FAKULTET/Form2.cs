8d9eaab652afed6a2eae575f08e797cc82e08188
Lz42 <staslz42stas@gmail.com>
2023-10-25 10:07:11 +0300

    AUTO FROM WORK 25.10.2023 10:07:10,73

---
+++ 4 kurs/СУБД/DUDKIN/FAKULTET/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Fakultet
+{
+    public partial class Form2 : Form
+    {
+        DataBase dataBase = new DataBase();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataBase.openConnection();
+            var addQuery = $"insert into [Group] (Name,Descr) values ('{textBox1.Text}' , '{textBox2.Text}')";
+            var command = new SqlCommand(addQuery, dataBase.getConnection());
+            command.ExecuteNonQuery();
+            dataBase.closeConnection();
+            this.Close();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
